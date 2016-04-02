450fdf093191eddd8d67905adc8fffc091f445b7
docevaad <tomcik.david@gmail.com>
2016-04-02 16:41:49 -0700

    Initial Commit.

---
+++ DnD.CharacterCreator.SqlServerDb/Character/Ability.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [Character].[Ability]
+(
+	[AbilityId] CHAR(3) NOT NULL PRIMARY KEY,
+	[Name] VARCHAR(20) NOT NULL
+)
---
+++ DnD.CharacterCreator.SqlServerDb/StaticData/Character/AbilityData.sql
@@ -0,0 +1,6 @@
+﻿INSERT INTO [Character].[Ability] ([AbilityId], [Name]) VALUES ('STR', 'Strength');
+INSERT INTO [Character].[Ability] ([AbilityId], [Name]) VALUES ('DEX', 'Dexterity');
+INSERT INTO [Character].[Ability] ([AbilityId], [Name]) VALUES ('CON', 'Constitution');
+INSERT INTO [Character].[Ability] ([AbilityId], [Name]) VALUES ('INT', 'Intelligence');
+INSERT INTO [Character].[Ability] ([AbilityId], [Name]) VALUES ('WIS', 'Wisdom');
+INSERT INTO [Character].[Ability] ([AbilityId], [Name]) VALUES ('CHA', 'Charisma');
